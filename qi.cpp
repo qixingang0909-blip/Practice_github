7df937a794ee9e59a1902d894f403c45dc05329a
qixingang <qixingang0909@gmail.com>
2025-10-24 00:23:45 +0800

    修改

--- qi.cpp
+++ qi.cpp
@@ -3,42 +3,14 @@
 int main() {
     std::cout << "Hello World!" << std::endl;
     std::cout << "Hello World!" << std::endl;
-    std::cout << "Hello World!" << std::endl;
-    std::cout << "Hello World!" << std::endl;
-    std::cout << "Hello World!" << std::endl;
-    std::cout << "Hello World!" << std::endl;
-    std::cout << "Hello World!" << std::endl;
-    std::cout << "Hello World!" << std::endl;
-    std::cout << "Hello World!" << std::endl;
-    std::cout << "Hello World!" << std::endl;
-    std::cout << "Hello World!" << std::endl;
-    std::cout << "Hello World!" << std::endl;    
-    std::cout << "Hello World!" << std::endl;
-    std::cout << "Hello World!" << std::endl;
-    std::cout << "Hello World!" << std::endl;
-    std::cout << "Hello World!" << std::endl;
-    std::cout << "Hello World!" << std::endl;
-    std::cout << "Hello World!" << std::endl;
-    std::cout << "Hello World!" << std::endl;
-    std::cout << "Hello World!" << std::endl;
-    std::cout << "Hello World!" << std::endl;
+
     std::cout << "Hello World!" << std::endl;
 
         std::cout << "Hello World!" << std::endl;
     std::cout << "Hello World!" << std::endl;
     std::cout << "Hello World!" << std::endl;    
     std::cout << "Hello World!" << std::endl;
-    std::cout << "Hello World!" << std::endl;
-    std::cout << "Hello World!" << std::endl;
-    std::cout << "Hello World!" << std::endl;
-    std::cout << "Hello World!" << std::endl;
-    std::cout << "Hello World!" << std::endl;
-    std::cout << "Hello World!" << std::endl;
-    std::cout << "Hello World!" << std::endl;
-    std::cout << "Hello World!" << std::endl;
-    std::cout << "Hello World!" << std::endl;
 
 
-    std::cout << "Hello World!" << std::endl;
     return 0;
 }

c71273d26ae5ce6895e9b637332e176eb86e5fc6
qixingang <qixingang0909@gmail.com>
2025-10-23 23:26:24 +0800

    修改

--- qi.cpp
+++ qi.cpp
@@ -8,20 +8,5 @@ int main() {
     std::cout << "Hello World!" << std::endl;
     std::cout << "Hello World!" << std::endl;
     std::cout << "Hello World!" << std::endl;
-    std::cout << "Hello World!" << std::endl;
-    std::cout << "Hello World!" << std::endl;
-    std::cout << "Hello World!" << std::endl;
-    std::cout << "Hello World!" << std::endl;
-    std::cout << "Hello World!" << std::endl;    std::cout << "Hello World!" << std::endl;
-    std::cout << "Hello World!" << std::endl;
-    std::cout << "Hello World!" << std::endl;
-    std::cout << "Hello World!" << std::endl;
-
-    std::cout << "Hello World!" << std::endl;
-    std::cout << "Hello World!" << std::endl;
-    std::cout << "Hello World!" << std::endl;
-    std::cout << "Hello World!" << std::endl;
-    std::cout << "Hello World!" << std::endl;
-    std::cout << "Hello World!" << std::endl;
     return 0;
 }

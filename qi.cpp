2417fde70de88ac3cbc1b2dacbbd1a635811cbf8
qixingang <qixingang0909@gmail.com>
2025-10-24 00:29:16 +0800

    推送

--- qi.cpp
+++ qi.cpp
@@ -5,12 +5,5 @@ int main() {
     std::cout << "Hello World!" << std::endl;
 
     std::cout << "Hello World!" << std::endl;
-
-        std::cout << "Hello World!" << std::endl;
-    std::cout << "Hello World!" << std::endl;
-    std::cout << "Hello World!" << std::endl;    
-    std::cout << "Hello World!" << std::endl;
-
-
     return 0;
 }

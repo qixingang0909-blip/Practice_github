32c5eafcf1928ef87f56e826702668a68da40624
qixingang <qixingang0909@gmail.com>
2025-10-25 21:34:04 +0800

    xiugai

--- qi.cpp
+++ qi.cpp
@@ -2,8 +2,8 @@
 
 int main() {
     std::cout << "Hello World!" << std::endl;
-    std::cout << "Hello World!" << std::endl;
-
+    std::cout << "apple!" << std::endl;
+    std::cout << "qixingang" << std::endl;
     std::cout << "qixingang" << std::endl;
     return 0;
 }

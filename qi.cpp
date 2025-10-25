57e8b9ae97efe50dee8deaa22a0b907ce93b1ba3
qixingang <qixingang0909@gmail.com>
2025-10-25 21:37:17 +0800

    修改

--- qi.cpp
+++ qi.cpp
@@ -3,7 +3,7 @@
 int main() {
     std::cout << "Hello World!" << std::endl;
     std::cout << "apple!" << std::endl;
-    std::cout << "qixingang" << std::endl;
-    std::cout << "qixingang" << std::endl;
+    std::cout << "qixingang2" << std::endl;
+    std::cout << "qixingang00000" << std::endl;
     return 0;
 }

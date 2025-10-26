3f78951e2a77a075a573c72e77d450969fb09692
qixingang <qixingang0909@gmail.com>
2025-10-26 19:06:44 +0800

    xiugai

---
+++ build/CMakeFiles/qi.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/qi.dir/qi.cpp.o"
+  "CMakeFiles/qi.dir/qi.cpp.o.d"
+  "qi"
+  "qi.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/qi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
